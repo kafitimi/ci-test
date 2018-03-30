ff16c8c03aeea88410dfc7793ee9b1cc0df8eaa9
Alexander Pavlov <7911562+av-pavlov@users.noreply.github.com>
2018-03-30 12:22:57 +0900

    broke on purpose

--- cplx.py
+++ cplx.py
@@ -1,6 +1,8 @@
 class Cplx:
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
     def r(self):
-        return (self.x**2 + self.y**2)**0.5
+        return 1+(self.x**2 + self.y**2)**0.5
