3383e1711902c57814b6068f8f6122ced4b149c5
Alexander Pavlov <7911562+av-pavlov@users.noreply.github.com>
2018-03-30 11:18:39 +0900

    add `self.` to assertTrue

--- tests/test_algebraic.py
+++ tests/test_algebraic.py
@@ -5,7 +5,7 @@ class TestCplxMethods(unittest.TestCase):
     
      def test_r(self):
          m = Cplx(1,1)
-         assertTrue(abs(m.r()- 2**0.5) < 0.0000001)
+         self.assertTrue(abs(m.r()- 2**0.5) < 0.0000001)
 
 
 
