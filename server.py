b9e46db72d64d0e4b491da207fba92c59ef77cc4
Mikhail Nikolaev <ivt201617@gmail.com>
2018-04-10 19:25:42 +0900

    UI changes

--- server.py
+++ server.py
@@ -6,15 +6,15 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def hello_world():
     if request.method == 'GET':
-        return '<h2>Введите вещ и мнимую часть комплексного числа</h2>' \
+        return '<h2>Введите вещественную и мнимую часть комплексного числа z</h2>' \
                '<form method="post">' \
-               'x<input id="x" name="x"><br>' \
-               'y<input id="y" name="y"><br>' \
+               'Вещественная часть, x <input id="x" name="x"><br>' \
+               'Мнимая часть, y <input id="y" name="y"><br>' \
                '<input type="submit">'\
                '</form>'
     else:
         x = float(request.values.get('x'))
         y = float(request.values.get('y'))
         z = Cplx(x,y)      
-        return '<h2>Аргумент комплексного числа z = '+str(x)+' + '+str(y)+'i</h2>' \
-               '<label>Аргумент </label>φ = '+str(z.arg())+' (рад)'
+        return '<h2>Комплексное число z = '+str(x)+' + '+str(y)+'i</h2>' \
+               '<label>Аргумент φ = '+str(z.arg())+' (рад)</label>'
