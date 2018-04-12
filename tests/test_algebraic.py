6ab2bbf7f1e5cefbb8b47cd46d17b7f0bcbcf4e4
Tit Novgorodov <Aivent201@gmail.com>
2018-04-12 23:06:42 +0900

    Fix 2

--- tests/test_algebraic.py
+++ tests/test_algebraic.py
@@ -6,10 +6,11 @@ class TestCplxMethods(unittest.TestCase):
      def test_r(self):
          m = Cplx(1,1)
          self.assertTrue(abs(m.r()- 2**0.5) < 0.0000001)
-	 def test_arg(self):
+     def test_arg(self):
          i = Cplx(1,1)
          self.assertTrue(i.arg()==0.7853981633974483)
-         print("new test")
+         print("new tests")
+
 
 
 if __name__=='__main__':
