91acd043d079b361d7976ce9346f0d7c99876147
Aigul <imalg95@gmail.com>
2018-04-13 03:14:39 +0900

    server.py + cplx.py

--- cplx.py
+++ cplx.py
@@ -1,3 +1,4 @@
+import math
 class Cplx:
 
     def __init__(self, x, y):
@@ -8,6 +9,17 @@ class Cplx:
         return (self.x**2 + self.y**2)**0.5
     def arg(self):
         if self.r():
-            return None
+            result=0
+            if self.x>0:
+                result=math.atan2(self.y, self.x)
+            if self.x<0 and self.y>=0:
+                result=math.pi+math.atan2(self.y, self.x)
+            if self.x<0 and self.y<0:
+                result=-math.pi+math.atan2(self.y, self.x)
+            if self.x==0 and self.y>0:
+                result=math.pi/2
+            if self.x==0 and self.y<0:
+                result=-math.pi/2
+            return result
         else:
-            return 0  # FIXME!
+            return 0
--- tests/test_algebraic.py
+++ tests/test_algebraic.py
@@ -1,13 +1,17 @@
 import unittest
 from cplx import *
 
+
 class TestCplxMethods(unittest.TestCase):
-    
-     def test_r(self):
-         m = Cplx(1,1)
-         self.assertTrue(abs(m.r()- 2**0.5) < 0.0000001)
+    def test_r(self):
+        m = Cplx(1, 1)
+        self.assertTrue(abs(m.r() - 2 ** 0.5) < 0.0000001)
+
+    def test_arg(self):
+        i = Cplx(1, 1)
+        self.assertTrue(i.arg() == 0.7853981633974483)
+        print("тест")
 
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     unittest.main()
