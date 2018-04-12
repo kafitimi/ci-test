382e00fb242c84e56578c3bdfa1fdee1415476a2
Alexey Neustroev <sselicrem@gmail.com>
2018-04-12 23:53:11 +0900

    arg func & server message

--- cplx.py
+++ cplx.py
@@ -6,8 +6,20 @@ class Cplx:
 
     def r(self):
         return (self.x**2 + self.y**2)**0.5
+
     def arg(self):
         if self.r():
-            return None
+            result = 0
+            if self.x > 0:
+                result = math.atan2(self.y, self.x)
+            if self.x < 0 and self.y >= 0:
+                result = math.pi + math.atan2(self.y, self.x)
+            if self.x < 0 and self.y < 0:
+                result = -math.pi + math.atan2(self.y, self.x)
+            if self.x == 0 and self.y > 0:
+                result = math.pi / 2
+            if self.x == 0 and self.y < 0:
+                result = -math.pi / 2
+            return result
         else:
-            return 0  # FIXME!
+            return 0
--- server.py
+++ server.py
@@ -13,4 +13,10 @@ def hello_world():
                '<input type="submit">'\
                '</form>'
     else:
-        return '<h1>Пока не реализовано</h1>'
+        x = float(request.values.get('x'))
+        y = float(request.values.get('y'))
+        z = Cplx(x, y)
+        return \
+            '<h2>Комплексное число z = '+str(x)+' + '+str(y)+'i</h2>' \
+            '<label>Модуль </label>r='+str(z.r())+'<br>'\
+            '<label>Аргумент </label>φ='+str(z.arg())+' (рад)'
