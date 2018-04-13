67685e300028e8c5c91a0f07ba6467239a9ae63f
Mikhail Nikolaev <ivt201617@gmail.com>
2018-04-13 15:48:39 +0900

    added tests for all if loops

--- cplx.py
+++ cplx.py
@@ -16,7 +16,7 @@ class Cplx:
 			if self.x < 0 and self.y >= 0:
 				result = math.atan2(self.y, self.x)
 			if self.x < 0 and self.y < 0:
-				result = -math.pi + math.atan2(self.y, self.x)
+				result = math.atan2(self.y, self.x)
 			if self.x == 0 and self.y>0:
 				result = math.pi/2
 			if self.x == 0 and self.y<0:
--- tests/test_algebraic.py
+++ tests/test_algebraic.py
@@ -8,8 +8,24 @@ class TestCplxMethods(unittest.TestCase):
 		self.assertTrue(abs(m.r() - 2**0.5) < 0.0000001)
 
 	def test_arg(self):
-		z = Cplx(1,1)
-		self.assertTrue(z.arg() == 0.7853981633974483)
+		z = Cplx(3,5)
+		self.assertTrue(z.arg() == 1.0303768265243124)
+		
+	def test_arg(self):
+		z = Cplx(-3,5)
+		self.assertTrue(z.arg() == 2.1112158270654807)
+		
+	def test_arg(self):
+		z = Cplx(-3,-5)
+		self.assertTrue(z.arg() == -2.1112158270654807)
+
+	def test_arg(self):
+		z = Cplx(0,5)
+		self.assertTrue(z.arg() == 1.5707963267948966)
+		
+	def test_arg(self):
+		z = Cplx(0,-5)
+		self.assertTrue(z.arg() == -1.5707963267948966)
 		print("end of testing")
 
 
